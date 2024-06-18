3d1b9d5cd20dd2109c43727787b56c0f9ad2f1d2
TheHusseler <bdhussel@gmail.com>
2024-06-18 15:32:29 -0500

    Preparing for release

---
+++ bot.py
@@ -0,0 +1,220 @@
+import discord
+from discord import app_commands
+from discord.ext import tasks, commands
+from config_manager import BotConfig, ConfigModal, ConfigView
+import datetime
+import json
+
+#Task is timed for 10 UTC, or 4 central
+cleanUpTimes = [datetime.time(hour=13, minute=30, tzinfo=datetime.timezone.utc)]
+
+intents=discord.Intents.default()
+intents.typing = False
+intents.presences = False
+intents.messages = True
+intents.members = True
+intents.reactions = True
+
+#Necessary Permissions
+#permissions integer (untested): 67202112
+
+# client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents,
+                   case_insensitive=False)
+
+bot_config = BotConfig()
+count = 0
+
+#Notifications for users leaving/joining
+@bot.event
+async def on_member_remove(member):
+    notifications_channel = bot_config.get_guild_data(str(member.guild.id), "NOTIFICATIONS_CHANNEL")
+    await bot.get_channel(notifications_channel).send(f"{member.mention} has left the server.")
+
+    # - 90 days till deletion, runs once a day
+    # - Where they want their logs
+
+def has_mod_role():
+    async def predicate(interaction: discord.Interaction):
+        guild_id = str(interaction.guild.id)
+        if not bot_config.config["GUILDS"].get(guild_id):
+            return True # Allow if guild is not in config
+        mod_role_id = bot_config.get_guild_data(guild_id, "MOD_ROLE")
+        if not mod_role_id:
+            return True  # Allow if mod_role is not set
+
+        mod_role = interaction.guild.get_role(int(mod_role_id))
+        if mod_role in interaction.user.roles:
+            return True
+        else:
+            await interaction.response.send_message("You do not have the required role to execute this command.", ephemeral=True)
+            return False
+    return app_commands.check(predicate)
+
+#Regular Message Deletion
+@bot.tree.command(
+    name="clean-up",
+    description="Cleans up messages that aren't saved and are over x days old."
+)
+@has_mod_role()
+async def clean_up(interaction):
+    await interaction.response.defer()  # Defer the response to prevent timeout
+    deleted = await clean_up_messages(interaction.guild_id)
+    try:
+        await interaction.followup.send(f"Deleted {deleted} messages by command for guild {interaction.guild_id}.")
+    except discord.errors.NotFound:
+        print("Interaction not found. Possibly due to timing issues.")
+
+async def clean_up_messages(guildId) -> int:
+    count = 0
+    messageAgeLimit = int(bot_config.get_guild_data(str(guildId), "MESSAGE_AGE_LIMIT"))
+    date = discord.utils.utcnow() - datetime.timedelta(hours=messageAgeLimit)
+    for channel in bot.get_guild(int(guildId)).text_channels:
+        try:
+            deleted = await channel.purge(limit=None, check=should_delete, before=date)
+            count += len(deleted)
+        except Exception as e:
+            print(f"Error deleting messages in channel {channel.id}: {e}")
+    for channel in bot.get_guild(int(guildId)).voice_channels:
+        try:
+            deleted = await channel.purge(limit=None, check=should_delete, before=date)
+            count += len(deleted)
+        except Exception as e:
+            print(f"Error deleting messages in voice channel {channel.id}: {e}")
+    for thread in bot.get_guild(int(guildId)).threads:
+        try:
+            deleted = await thread.purge(limit=None, check=should_delete, before=date)
+            count += len(deleted)
+        except Exception as e:
+            print(f"Error deleting messages in thread {thread.id}: {e}")
+    return count
+
+def should_delete(message) -> bool:
+    global count
+    age = discord.utils.utcnow() - message.created_at
+    guildId = str(message.guild.id)
+    deleteBotMessages = (bot_config.get_guild_data(guildId, "DELETE_BOT_MESSAGES") == "True")
+    saveEmojiName = bot_config.get_guild_data(guildId, "SAVE_EMOJI_NAME")
+    return (not message.pinned 
+        and not has_save_emoji(message, saveEmojiName) 
+        and (message.author != bot.user or deleteBotMessages)
+        and not message.is_system())
+
+def has_save_emoji(message, saveEmojiName) -> bool:
+    for reaction in message.reactions:
+        if reaction.is_custom_emoji() and reaction.emoji.name == saveEmojiName:
+            return True
+    return False
+
+## Logging and Bot Specifics
+
+#Log data about save reactions
+@bot.event
+async def on_raw_reaction_add(payload):
+    msg_channel = bot.get_channel(payload.channel_id)
+    msg = await msg_channel.fetch_message(payload.message_id)
+    user = bot.get_user(payload.user_id)
+    save_emoji_name = bot_config.get_guild_data(str(payload.guild_id), "SAVE_EMOJI_NAME")
+    if (payload.emoji.name == save_emoji_name):
+        embedVar = discord.Embed(title=f"User {payload.member.display_name} saved post", color=0x00ff00)
+        embedVar.add_field(name="Original Author", value=f"{msg.author.display_name}", inline=True)
+        embedVar.add_field(name="Saved By", value=f"{user.display_name}", inline=True)
+        embedVar.add_field(name="Link", value=f"{msg.jump_url}", inline=True)
+        embedVar.add_field(name="Time Saved", value=f"{datetime.datetime.now()}", inline=True)
+        bot_logs_channel = bot_config.get_guild_data(str(payload.guild_id), "BOT_LOGS_CHANNEL")
+        await bot.get_channel(bot_logs_channel).send(embed=embedVar)
+
+#Logging bot details
+@bot.event
+async def on_ready():
+    guild_data = {}
+    bot_config.load_config("config.json")
+    await bot.add_cog(DailyAction(bot, bot_config))
+    # clean_up_messages_daily.start()
+
+    await logAll("Bot is ready and running.")
+    
+@bot.event
+async def on_resumed():
+    await logAll("Bot has reconnected.")
+    print("Bot has reconnected.")
+
+
+@bot.event
+async def on_disconnect():
+    await logAll("Bot has disconnected.")
+
+@bot.tree.error
+async def on_app_command_error(interaction: discord.Interaction, error):
+    if isinstance(error, app_commands.CheckFailure):
+        # Ignore CheckFailure errors
+        return
+    raise error 
+
+async def logAll(message):
+    try:
+        for guild_id, guild in bot_config.config["GUILDS"].items():
+            await log(message, guild_id)
+    except KeyError as e:
+        print(f"KeyError: {e}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+
+async def log(message, guildId):
+    try:
+        channel = bot_config.get_guild_data(guildId, "BOT_LOGS_CHANNEL")
+        if channel:
+            await bot.get_channel(channel).send(message)
+        else:
+            print(f"Channel not found for guild {guildId}")
+    except Exception as e:
+        print(f"Failed to log message to guild {guildId}: {e}")
+
+#Command to check if bot is running
+@bot.tree.command(
+    name="bot-check",
+    description="Check if bot is running"
+)
+@has_mod_role()
+async def bot_check(interaction):
+    await interaction.response.send_message("Your bot is running! Better go catch it...", ephemeral=True)
+
+@bot.tree.command(name="config", description="Configure the bot settings.")
+@has_mod_role()
+async def config(interaction: discord.Interaction):
+    view = ConfigView(interaction.guild, bot_config)
+    description = "Select some configuration values for the bot. Then hit next to open the modal for more configuration values.\
+     \nMod Role - The role that has permission to use the bot's commands. \nNotifications - Where the bot will send notifications for users joining/leaving. \
+     \nBot Logs - Where the bot will errors, statuses and message saves. \n Delete Bot Messages - Whether the bot will delete it's own messages."
+    embed = discord.Embed(title="Configure Bot Settings", description=description)
+    await interaction.response.send_message("Configure the bot settings:", embed=embed, view=view, ephemeral=True)
+
+@bot.tree.command(name="sync", description="Sync the command tree.")
+@has_mod_role()
+async def sync(interaction: discord.Interaction):
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands")
+        await interaction.response.send_message(f'Command tree synced. {len(synced)} commands synchronized.', ephemeral=True)
+    except Exception as e:
+        print(f"Failed to sync: {e}")
+        await interaction.response.send_message(f'Failed to sync commands: {e}', ephemeral=True)
+
+class DailyAction(commands.Cog):
+    def __init__(self, bot, bot_config) -> None:
+        self.bot = bot
+        self.bot_config = bot_config
+        self.clean_up_messages_daily.start()
+
+    @tasks.loop(time=cleanUpTimes)
+    async def clean_up_messages_daily(self) -> None:
+        print("Daily clean-up job")
+        guilds = self.bot_config.config["GUILDS"].items();
+        for guild_id, guild in guilds:
+            guild_id_str = str(guild_id)
+            messageAgeLimit = int(self.bot_config.get_guild_data(guild_id_str, "MESSAGE_AGE_LIMIT"))
+            await log(f"Cleaning up messages that aren't saved and are over {messageAgeLimit} days old.", guild_id_str)
+            deleted = await clean_up_messages(guild_id)
+            await log(f"Deleted {deleted} messages", guild_id_str)
+
+bot.run(BOT_TOKEN)
---
+++ config_manager.py
@@ -0,0 +1,163 @@
+
+import json
+import discord
+from discord import SelectOption
+from discord.ui import View, Button, Modal, TextInput, ChannelSelect, Select, RoleSelect
+from discord.ext import commands
+
+#Config structure:
+#{
+#     "GUILDS": {
+#         "GUILD ID as key": {
+#             "MOD_ROLE": role id, role for moderators
+#             "NOTIFICATIONS_CHANNEL": channel id, Notifications for when a user leaves or other misc notifications
+#             "BOT_LOGS_CHANNEL": channel id, logs details for the bot's functionality and debugging
+#             "SAVE_EMOJI_NAME": name of emoji for saving posts,
+#             "MESSAGE_AGE_LIMIT": 90 by default
+#             "DELETE_BOT_MESSAGES": True/False
+#         }
+#     },
+#     "BOT_TOKEN": "Token Here"
+
+class BotConfig:
+    def __init__(self):
+        self.config = {}
+
+    def load_config(self, file_path):
+        try:
+            with open(file_path, "r") as file:
+                self.config = json.load(file)
+            print(f"Loaded config")
+        except FileNotFoundError:
+            print("config.json file not found.")
+        except json.JSONDecodeError:
+            print("Error decoding config.json.")
+        except Exception as e:
+            print(f"Unexpected error loading config.json: {e}")
+
+    def save_config(self, file_path):
+        try:
+            with open(file_path, "w") as file:
+                json.dump(self.config, file, indent=4)
+            print(f"Config saved")
+        except Exception as e:
+            print(f"Unexpected error saving config.json: {e}")
+
+    def get_guild_data(self, guild_id, data_name):
+        guild = self.config["GUILDS"][guild_id]
+        return guild[data_name]
+
+
+class ConfigModal(Modal):
+    bot_config: BotConfig
+
+    title = "Configure Bot - Part 2"
+
+    def __init__(self, bot_config: BotConfig):
+        super().__init__(title="Configure Bot - Part 2")
+
+        self.bot_config = bot_config
+
+        self.add_item(TextInput(label="Save Emoji Name", placeholder="Enter the name of the emoji used to save posts from deletion"))
+        self.add_item(TextInput(label="Message Age Limit", placeholder="Enter the number of days you want to keep messages for"))
+
+    async def on_submit(self, interaction: discord.Interaction):
+        try:
+            guild_id = str(interaction.guild.id)
+
+            try:
+                # Validate message age limit input
+                message_age_limit = int(self.children[1].value)
+            except ValueError:
+                await interaction.response.send_message("Invalid input for Message Age Limit. Please enter a number.", ephemeral=True)
+                return
+
+            guild_data = {
+                "SAVE_EMOJI_NAME": self.children[0].value,
+                "MESSAGE_AGE_LIMIT": int(self.children[1].value)
+            }
+            if guild_id in self.bot_config.config["GUILDS"]: 
+                self.bot_config.config["GUILDS"][guild_id].update(guild_data)
+            else:
+                self.bot_config.config["GUILDS"][guild_id] = guild_data
+            
+            self.bot_config.save_config("config.json")
+
+            await interaction.response.send_message("Configuration updated successfully!", ephemeral=True)
+        except Exception as e:
+            print(f"Error in ConfigModal callback: {e}")
+            await interaction.response.send_message("An error occurred. Please try again.", ephemeral=True)
+
+
+
+class ConfigView(View):
+    bot_config: BotConfig
+
+    def __init__(self, guild: discord.Guild, bot_config: BotConfig):
+        super().__init__()
+
+        self.bot_config = bot_config
+
+        self.mod_role_select = RoleSelect(
+            placeholder="Select Moderator Role",
+            custom_id="mod_role_select"
+        )
+        
+        self.notifications_channel_select = ChannelSelect(
+            placeholder="Select Notifications Channel",
+            custom_id="notifications_channel_select",
+            channel_types=[discord.ChannelType.text]
+        )
+
+        self.bot_logs_channel_select = ChannelSelect(
+            placeholder="Select Bot Logs Channel",
+            custom_id="bot_logs_channel_select",
+            channel_types=[discord.ChannelType.text]
+        )
+
+        self.delete_bot_messages = Select(placeholder="Delete Bot Messages", 
+            options=[
+                SelectOption(label="Yes", description="The bot will delete it's own messages", value=True), 
+                SelectOption(label="No", description="The bot will never delete it's own messages", value=False)
+            ]
+        )
+
+        self.add_item(self.mod_role_select)
+        self.add_item(self.notifications_channel_select)
+        self.add_item(self.bot_logs_channel_select) 
+        self.add_item(self.delete_bot_messages)    
+
+    async def interaction_check(self, interaction: discord.Interaction):
+        if interaction.data.get('custom_id') != 'next':
+            await interaction.response.defer()
+        return True
+
+    @discord.ui.button(label="Next", style=discord.ButtonStyle.primary, custom_id="next", row=4)
+    async def next(self, interaction: discord.Interaction, button: discord.ui.Button):
+        guild_id = str(interaction.guild.id)
+
+        try:
+            guild_data = {
+                "MOD_ROLE": int(self.mod_role_select.values[0].id),
+                "NOTIFICATIONS_CHANNEL": int(self.notifications_channel_select.values[0].id),
+                "BOT_LOGS_CHANNEL": int(self.bot_logs_channel_select.values[0].id),
+                "DELETE_BOT_MESSAGES": self.delete_bot_messages.values[0]
+            }
+
+            if guild_id in self.bot_config.config["GUILDS"]:
+                self.bot_config.config["GUILDS"][guild_id].update(guild_data)
+            else:
+                self.bot_config.config["GUILDS"][guild_id] = guild_data
+
+            self.bot_config.save_config("config.json")
+
+            await interaction.response.send_modal(ConfigModal(self.bot_config))
+        except IndexError:
+            await interaction.response.send_message("Must select a channel for each", ephemeral=True)
+            return
+        except Exception as e:
+            await interaction.response.send_message(f"Unexpected error saving selections: {e}", ephemeral=True)
+            return
+
+
+
